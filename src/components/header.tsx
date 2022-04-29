21ab9966d0594bb48a3fb0f5f5c155c7c929239c
Yuya Ohbayashi <yuya9445@gmail.com>
2022-04-29 13:02:49 +0900

    rename to oh884U8.blog

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,12 +19,12 @@ const Header = ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} oh884U8.blog</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
-        <meta name="og:title" content="My Notion Blog" />
+        <meta name="og:title" content="oh884U8.blog" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
--- src/lib/build-rss.ts
+++ src/lib/build-rss.ts
@@ -68,7 +68,7 @@ function createRSS(blogPosts = []) {
     <link href="/atom" rel="self" type="application/rss+xml"/>
     <link href="/" />
     <updated>${NOW}</updated>
-    <id>My Notion Blog</id>${postsString}
+    <id>oh884U8.blog</id>${postsString}
   </feed>`
 }
 
