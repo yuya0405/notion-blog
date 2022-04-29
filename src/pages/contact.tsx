56f3ec5e418e3f1121983d54e4aeada284703bd5
Yuya Ohbayashi <yuya9445@gmail.com>
2022-04-29 12:44:50 +0900

    update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
       <Header titlePre="Contact" />
       <div className={sharedStyles.layout}>
         <div className={contactStyles.avatar}>
-          <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
+          <img src="/penguin.png" alt="avatar with letters JJ" height={60} />
         </div>
 
         <h1 style={{ marginTop: 0 }}>Contact</h1>
